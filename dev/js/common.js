421da1a510526f5b208537516aff8e33ccd2e710
Boris Tayoursky <borik.hub@gmail.com>
2017-03-04 03:17:04 +0900

    add media responsive and fixed little

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,9 +1,31 @@
+$(document).ready(function() {
+
+
+$(".nav_button").click(function() {
+        $(".header_nav ul").slideToggle();
+    });
+
+
 // console.log("nextprev");
 var owl = $('.owl-carousel');
 owl.owlCarousel({
 	loop:true,
 	margin:10,
-	items : 4
+	items : 4,
+	responsive:{
+        800:{
+            items:4
+        },
+        650:{
+            items:3
+        },
+        480:{
+            items:2
+        },
+        0:{
+            items:1
+        },
+   }
 });
 $('.next_button').click(function() {
     owl.trigger('next.owl.carousel');
@@ -12,3 +34,6 @@ $('.prev_button').click(function() {
     
     owl.trigger('prev.owl.carousel');
 });
+
+
+});//end begin
